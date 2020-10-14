7dadbcd8c7ab97844f80173693c5d6db8ba1e5d2
Sanket Berde <sanketberde1991@gmail.com>
2020-10-14 18:15:48 +0530

    react-create-app on Heroku

---
+++ build/precache-manifest.5b4c5418672efaceefead79623589a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3a71e267d5d215b9b63757e3dd622a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1fe4554e7d198d89edd",
+    "url": "/static/css/main.2a100c38.chunk.css"
+  },
+  {
+    "revision": "60ad2f93da2b14fa1ed4",
+    "url": "/static/js/2.ad825f3a.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/static/js/2.ad825f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1fe4554e7d198d89edd",
+    "url": "/static/js/main.a0048f1b.chunk.js"
+  },
+  {
+    "revision": "7c5ad9155f518db1b693",
+    "url": "/static/js/runtime-main.c1a20747.js"
+  }
+]);
